b4648b4522c2aa660e066d8e0b34d01fd4906423
Dan Ryan <dan@danryan.co>
2018-11-11 17:14:53 -0500

    Code cleanup
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -74,7 +74,7 @@ def test_nonblocking_run():
     assert c.returncode == 0
     c.wait()
     assert "PYTHONDONTWRITEBYTECODE" in c.out, c.out
-    out, err = vistir.misc.run(["python", "--help"], block=False, nospin=True)
+    out, _ = vistir.misc.run(["python", "--help"], block=False, nospin=True)
     assert "PYTHONDONTWRITEBYTECODE" in out, out
     # historical = []
     # while out:
