9c6556928e318cefa57a6c2c1aa1caae90173b2a
Dan Ryan <dan@danryan.co>
2019-05-17 20:40:48 -0400

    Expand variables before normalizing
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/vistir/path.py
+++ src/vistir/path.py
@@ -103,16 +103,13 @@ def normalize_path(path):
     :rtype: str
     """
 
+    path = os.path.abspath(os.path.expandvars(os.path.expanduser(str(path))))
     if os.name == "nt":
         from ._winconsole import get_long_path
 
         path = get_long_path(path)
 
-    return os.path.normpath(
-        os.path.normcase(
-            os.path.abspath(os.path.expandvars(os.path.expanduser(str(path))))
-        )
-    )
+    return os.path.normpath(os.path.normcase(path))
 
 
 def is_in_path(path, parent):
