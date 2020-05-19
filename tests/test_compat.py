b0a20f2f0d2b31a2c1d37dfdcc696cf86ee1f21c
Dan Ryan <dan.ryan@canonical.com>
2020-05-19 17:10:21 -0400

    Add test to make sure samefile works on all platforms
    Signed-off-by: Dan Ryan <dan.ryan@canonical.com>

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -13,3 +13,9 @@ from .strategies import legal_path_chars
 @example(path=u"unicode\u0141")
 def test_decode_encode(path):
     assert vistir.compat.fs_decode(vistir.compat.fs_encode(path)) == path
+
+
+def test_samefile(tmpdir):
+    tmp_file = tmpdir.join("testfile.txt")
+    tmp_file.write_text(u"asdf", encoding="utf-8")
+    assert vistir.compat.samefile(str(tmp_file), tmp_file.strpath) is True
