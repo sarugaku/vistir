7664fc588adf09fb99d8cbc51316d4db7f859c61
Matt Davis <matteius@gmail.com>
2022-07-27 19:21:44 -0400

    Release 0.5.5

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -98,7 +98,7 @@ def _render_log():
         False,  # Don't add newlines to wrapped text.
         {
             "name": "vistir",
-            "version": "0.5.4",
+            "version": "0.5.5",
             "date": "2022-7-27",
         },  # towncrier==19.9.0
     )
