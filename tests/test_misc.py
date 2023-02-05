700b78a8483f19fbb07d870d750d47d36d5828e0
Oz Tiram <oz.tiram@gmail.com>
2023-02-05 13:18:55 +0100

    Remove tests for deprecated and dropped functions

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -36,63 +36,6 @@ def test_shell_escape():
     assert vistir.misc.shell_escape(escaped_python) == expected
 
 
-@given(
-    st.lists(st.integers(min_value=1), min_size=1),
-    st.lists(
-        st.lists(st.integers(min_value=1, max_value=9999999999), min_size=1),
-        min_size=1,
-        max_size=5,
-    ),
-)
-def test_unnest(seed_ints, additional_lists):
-    assume(len(additional_lists) > 0 and len(seed_ints) > 0)
-    composite_list = seed_ints[:]
-    flattened_list = seed_ints[:]
-    flattened_list.extend(list(itertools.chain.from_iterable(additional_lists)))
-    list_copies = additional_lists[:]
-    for i in reversed(range(len(additional_lists))):
-        if len(additional_lists[i]) > 0:
-            if not isinstance(additional_lists[i], int):
-                if i > 0:
-                    list_copies[i - 1].append(additional_lists[i])
-                else:
-                    composite_list.append(list_copies[i])
-    assert all(
-        (isinstance(x, int) or x.isdigit()) for x in flattened_list
-    ), flattened_list
-    assert sorted(list(vistir.misc.unnest(composite_list))) == sorted(flattened_list)
-
-
-@pytest.mark.parametrize(
-    "iterable, result",
-    [
-        [["abc", "def"], True],
-        [("abc", "def"), True],
-        ["abcdef", True],
-        [None, False],
-        [1234, False],
-    ],
-)
-def test_is_iterable(iterable, result):
-    assert vistir.misc._is_iterable(iterable) is result
-
-
-def test_unnest_none():
-    assert list(vistir.misc.unnest(None)) == [None]
-
-
-def test_dedup():
-    dup_strings = ["abcde", "fghij", "klmno", "pqrst", "abcde", "klmno"]
-    assert list(vistir.misc.dedup(dup_strings)) == [
-        "abcde",
-        "fghij",
-        "klmno",
-        "pqrst",
-    ]
-    dup_ints = (12345, 56789, 12345, 54321, 98765, 54321)
-    assert list(vistir.misc.dedup(dup_ints)) == [12345, 56789, 54321, 98765]
-
-
 def test_get_stream_results():
     class MockCmd(object):
         def __init__(self, stdout, stderr):
